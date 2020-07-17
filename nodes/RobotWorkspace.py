72f62600c67b3b08c21eea754dbb415453e98811
G-Conard <conardg@lafayette.edu>
2020-07-17 16:52:37 -0400

    Added FK equations to RobotWorkspace, still have to test

--- nodes/RobotWorkspace.py
+++ nodes/RobotWorkspace.py
@@ -1,10 +1,28 @@
 from numpy import *
 from matplotlib.pyplot import *
 
+## TODO: Test to see if ANY of this is actually right (in spyder?)
+
+# Limb dimensions
+# From inside face of hip near hip servo to inside face near femur servo (inch)
+hipY = 1.35;
+hipZ = 0.568 + 0.25;
+hipX = 0.36
+
+# From inside face of hip near femur servo to inside face of tibia near tibia servo
+femurLength = 1.715; # inch
+
+# From inside face of tibia near tibia servo to foot
+tibiaLength = 2.786; # inch
+
+# Initialize possible foot position arrays
+all_xp = []
+all_yp = []
+all_zp = []
 
 # Initialize allowable angle ranges for each joint
-hipJRangeMin = 65
-hipJRangeMax = 92
+hipJRangeMin = 88
+hipJRangeMax = 115
 femurJRangeMin = 0
 femurJRangeMax = 150
 tibiaJRangeMin = 0
@@ -36,13 +54,32 @@ for h in hipRange:
     # position of femur joint relative to hip in neutral position
 
     # Relative change due to angle (won't affect x)
+    x2 = - hipX
+    y2 = hipY * sin(deg2rad(h)) + hipZ * sin(deg2rad(90-h))
+    z2 = hipY * cos(deg2rad(h)) - hipZ * cos(deg2rad(90-h))
 
     for f in femurRange:
         # Calculate location of tibia joint
         # length of femur
 
-        # x, y, z position (look at ME240 notes?)
+        # x, y, z position
+        x3 = x2 + femurLength * cos(-f)
+        # y3 = y2 + femurLength * sin(h)
+        z3_prime = femurLength * sin(-f)
 
         for t in tibiaRange:
             # Calculate location of foot
             # length of tibia (approx or actual?)
+
+            xp_prime = tibiaLength * cost(t)
+            z4_prime = tibiaLength * sin(t)
+
+            xp = x3 + xp_prime *cos(-f) - z4_prime * sin(-f)
+            z4 = z3_prime + xp_prime*sin(-f) + z4_prime*cos(-f)
+
+            zp = z2 + z4*cos(90-h)
+            yp = y2 - z4 * sin(90-h)
+
+            all_xp.append(xp)
+            all_yp.append(yp)
+            all_zp.append(zp)
